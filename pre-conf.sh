af69c30747cb1a669a5167cc92399f0c09f793a4
Aslan <mangixlax123@gmail.com>
2023-03-22 14:48:41 +0300

    Name chnaged to sharptime

--- pre-conf.sh
+++ pre-conf.sh
@@ -7,13 +7,13 @@ cp ./src/.env.example ./src/.env
 
 echo "USER=$USER" >> ./docker/.env
 
-docker-compose up -d
+docker compose up -d
 
-docker-compose exec backend composer install
-docker-compose exec backend php artisan key:generate
-docker-compose exec backend php artisan october:migrate
-docker-compose exec backend php artisan october:update
+docker compose exec backend composer install
+docker compose exec backend php artisan key:generate
+docker compose exec backend php artisan october:migrate
+docker compose exec backend php artisan october:update
 
-docker-compose down
+docker compose down
 
 echo "Configuration setup completed"
